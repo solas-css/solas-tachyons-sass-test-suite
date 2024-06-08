202c007be35c55ddd06cf7d7daed697d027fd546
Benjamin Rush <bnjmnrsh@gmail.com>
2024-06-08 23:19:35 +0100

    Subject: Refactor Reason for Commit: Broken. Summary: Corrected module.exports, viewports.map uses viewport objects from basicConfig, linting.

--- tests/mainConfig.js
+++ tests/mainConfig.js
@@ -1,54 +1,40 @@
-const basicConfig = require("./basicConfig");
+const basicConfig = require('./basicConfig.js')
 
-const THREE_SECONDS_IN_MS = 3000;
-const scenarios = [];
-const viewports = [];
+const THREE_SECONDS_IN_MS = 3000
+const scenarios = []
+const viewports = []
 
+// Add relativeUrls from basicConfig
 basicConfig.relativeUrls.map((relativeUrl) => {
-  scenarios.push({
+  return scenarios.push({
     label: relativeUrl,
     url: `${basicConfig.baseUrl}${relativeUrl}`,
-    referenceUrl: "",
-    readyEvent: "",
-    readySelector: "",
+    referenceUrl: '',
+    readyEvent: '',
+    readySelector: '',
     delay: THREE_SECONDS_IN_MS,
     hideSelectors: [],
     removeSelectors: [],
-    hoverSelector: "",
-    clickSelector: "",
+    hoverSelector: '',
+    clickSelector: '',
     postInteractionWait: 0,
     selectors: [],
     selectorExpansion: true,
     expect: 0,
     misMatchThreshold: 0.1,
-    requireSameDimensions: false,
-  });
-});
+    requireSameDimensions: false
+  })
+})
 
-basicConfig.viewports.map((viewport) => {
-  if (viewport === "small") {
-    pushViewport(viewport, 240, 500);
-  }
-  if (viewport === "medium") {
-    pushViewport(viewport, 500, 800);
-  }
-  if (viewport === "large") {
-    pushViewport(viewport, 800, 1280);
-  }
-  if (viewport === "1080v") {
-    pushViewport(viewport, 1920, 1080);
-  }
-  if (viewport === "4Kv") {
-    pushViewport(viewport, 2560, 1440);
-  }
-});
+// Add viewports from basicConfig
+basicConfig.viewports.map((vp) => pushViewport(vp.key, vp.height, vp.width))
 
 function pushViewport(viewport, width, height) {
   viewports.push({
     name: viewport,
     width,
-    height,
-  });
+    height
+  })
 }
 
 module.exports = {
@@ -56,19 +42,19 @@ module.exports = {
   viewports,
   scenarios,
   paths: {
-    bitmaps_reference: "tests/backstop_data/bitmaps_reference",
-    bitmaps_test: "tests/backstop_data/bitmaps_test",
-    engine_scripts: "tests/backstop_data/engine_scripts",
-    html_report: "tests/backstop_data/html_report",
-    ci_report: "tests/backstop_data/ci_report",
+    bitmaps_reference: 'tests/backstop_data/bitmaps_reference',
+    bitmaps_test: 'tests/backstop_data/bitmaps_test',
+    engine_scripts: 'tests/backstop_data/engine_scripts',
+    html_report: 'tests/backstop_data/html_report',
+    ci_report: 'tests/backstop_data/ci_report'
   },
-  report: ["browser"],
-  engine: "puppeteer",
+  report: ['browser'],
+  engine: 'puppeteer',
   engineOptions: {
-    args: ["--no-sandbox"],
+    args: ['--no-sandbox']
   },
   asyncCaptureLimit: 5,
   asyncCompareLimit: 50,
   debug: false,
-  debugWindow: false,
-};
+  debugWindow: false
+}
