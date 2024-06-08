43510ef75bf4ccefec2d5ee526bfef8a82bf653a
Benjamin Rush <bnjmnrsh@gmail.com>
2024-06-08 23:02:00 +0100

    Subject: Updated basicConfig Reason for Commit: Broken Summary: Proper exports, use of ./public/ as base url (no need for http call) or PatternLab server. Also created explicit viewports object.

--- tests/basicConfig.js
+++ tests/basicConfig.js
@@ -1,11 +1,19 @@
-const baseUrl = "http://127.0.0.1:3000"; // Replace the value "http://example.com" by the base URL of the website you want to test.
-const projectId = "tachyons-sass-haru"; // Replace the value "sample project" by the id of your project. It can be any string (e.g., "my-website").
-
-const viewports = ["small", "medium", "large", "full"];
-
-basicConfig.relativeUrls = [
-  ...require("./baseTypography"),
-  ...require("./baseLayout"),
-  ...require("./baseThemeing"),
-  ...require("./baseElements"),
-];
+const basicConfig = {
+  //   baseUrl: 'http://127.0.0.1:3000/', // Is PatternLab Server, or could be our own site.
+  baseUrl: './public/', // Paths are relative to curent working directory.
+  projectId: 'tachyons-sass-haru', // Replace the value "sample project" by the id of your project. It can be any string (e.g., "my-website").
+  viewports: [
+    { key: 'small', width: 240, height: 500 },
+    { key: 'medium', width: 500, height: 800 },
+    { key: 'large', width: 800, height: 1280 }
+    // { key: '1080v', width: 1920, height: 1080 },
+    // { key: '1440v', width: 2560, height: 1440 }
+  ],
+  relativeUrls: [
+    ...require('./paths/baseTypography'),
+    ...require('./paths/baseLayout'),
+    ...require('./paths/baseTheming'),
+    ...require('./paths/baseElements')
+  ]
+}
+module.exports = basicConfig
